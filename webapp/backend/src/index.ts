892a9e16a396863c07378397fbc8526ea1b1148d
John <117869184+TamerJohn@users.noreply.github.com>
2025-03-13 01:03:33 +0300

    added '/logs' endpoint for logs, probably won't be used with grafana

--- webapp/backend/src/index.ts
+++ webapp/backend/src/index.ts
@@ -1,15 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import pg, { Pool } from 'pg'
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hit on our root page!')
+const pool = new Pool()
+
+app.get('/logs', async (req, res) => {
+  const logs = await pool.query('SELECT * FROM logs')
+  res.send(logs.rows)
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log('App started listening!')
 })
