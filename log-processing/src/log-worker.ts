249a86beb9e28e6a5082444f1c45077d05960caf
astout55555 <alex.the.stout5@gmail.com>
2025-03-14 15:57:09 -0700

    feat: connect worker to containerized queue and DB

--- log-processing/src/log-worker.ts
+++ log-processing/src/log-worker.ts
@@ -1,25 +1,45 @@
 import { Worker, Job } from 'bullmq';
 import { redisConnection } from './redis-connection';
+import pg from 'pg';
 
-// Test transformation function
-const transformLog = (log: any) => {
+const { Pool } = pg;
+
+const db = new Pool({
+  connectionString: 'postgresql://postgres:secret@localhost:5469/unilogs_prepod',
+});
+
+// define object shape for every custom log transformation
+interface TransformedLog0 {
+  time: any,
+  level: any,
+  original_log: string
+}
+
+// test transformation function given 0, increment for each custom func
+const transformLog0 = (log: any): TransformedLog0 => {
   return {
-    timestamp: new Date().toISOString(),
+    time: log.time || new Date().toISOString(),
     level: log.level || 'info',
-    message: log.message || 'test message',
-    service: log.service || 'unknown',
+    original_log: 'REDACTED',
   };
 };
 
 const logWorker = new Worker(
   'logQueue',
   async (job: Job) => {
-    console.log(`Processing log: ${job.data}`);
+    console.log(`Processing batch logs job...`);
 
     // test transformation
-    const transformedLog = transformLog(job.data);
+    const transformedLogs: TransformedLog0[] = job.data.map((log: any) => transformLog0(log));
 
-    return transformedLog;
+    const sqlInsert = `INSERT INTO logs (time, body, original_log, level)
+      VALUES ($1, $2::jsonb, $3, $4);`;
+
+    transformedLogs.forEach((tLog: TransformedLog0) => {
+      db.query(sqlInsert, [tLog.time, tLog, tLog.original_log, tLog.level]);
+    });
+
+    return transformedLogs;
   },
   {
     connection: redisConnection,
@@ -38,5 +58,5 @@ logWorker.on('error', (error) => {
 
 // dev only, remove for prod
 logWorker.on('completed', (job: Job, returnvalue: any) => {
-  console.log('Job completed! Transformed log: ', returnvalue);
+  console.log('Job completed! Transformed logs: ', returnvalue);
 });
