5ca0127f90536296b60597b71a8a13a670606d15
Nathan Gross <nathan.gross@gmail.com>
2025-04-01 11:14:20 -0500

    feat: starting to add the ui

--- configuration_generator/build/main.js
+++ configuration_generator/build/main.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
-
 import prompts from "prompts";
 import gradient from "gradient-string";
 // import { ConfigGenerator } from "./ConfigGenerator";
-
 const logo = `                            ██           ████    
                           ██▓██      ███████     
              ▒░▒███████████  ███ █████▓███       
@@ -28,34 +26,30 @@ const logo = `                            ██           ████
         ▒▒    ▒▒          ██▓████                
                            █████                 
                            ███                   
-                           ██                    `;                                                                                     
-
-
+                           ██                    `;
 // const configGenerator = new ConfigGenerator();
-
 async function getMenuChoice() {
-  return await prompts({
-    type: 'select',
-    name: 'menuChoice',
-    message: 'What do you want to do?',
-    choices: [
-      { title: 'Add source', value: 'add_source'},
-      { title: 'Add sink', value: 'add_sink'},
-      { title: 'Generate vector_shipper.yaml', value: 'generateYaml'},
-      { title: 'Docker build and run shipper', value: 'buildAndRun'},
-      { title: 'Exit', value: 'exit'}
-    ]
-  });
+    return await prompts({
+        type: 'select',
+        name: 'menuChoice',
+        message: 'What do you want to do?',
+        choices: [
+            { title: 'Add source', value: 'add_source' },
+            { title: 'Add sink', value: 'add_sink' },
+            { title: 'Generate vector_shipper.yaml', value: 'generateYaml' },
+            { title: 'Docker build and run shipper', value: 'buildAndRun' },
+            { title: 'Exit', value: 'exit' }
+        ]
+    });
 }
-
 async function main() {
-  let notDone = true;
-  while (notDone) {
-    console.clear();
-    console.log(gradient(['aqua', 'purple']).multiline(logo));
-    const action = await getMenuChoice();
-    if (action.menuChoice === 'exit') notDone = false;
-  }
+    let notDone = true;
+    while (notDone) {
+        console.clear();
+        console.log(gradient(['aqua', 'purple']).multiline(logo));
+        const action = await getMenuChoice();
+        if (action.menuChoice === 'exit')
+            notDone = false;
+    }
 }
-
 void main();
