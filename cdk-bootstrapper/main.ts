8c0421018dfa80c3799dae10bcc219a80c2c6743
Nathan Gross <nathan.gross@gmail.com>
2025-04-04 11:46:40 -0500

    feat: bootstrap stack creation from node

--- cdk-bootstrapper/main.ts
+++ cdk-bootstrapper/main.ts
@@ -1,8 +1,15 @@
+import { CreateStackCommand } from "@aws-sdk/client-cloudformation";
 import createCloudFormationClient from "./createCloudFormationClient";
+import { readFileSync } from 'fs';
 
 async function main() {
+  const bootstrapTemplate = readFileSync('./bootstrap-template.yaml', 'utf8');
   const cloudFormationClient = await createCloudFormationClient();
-  console.log(cloudFormationClient);
+  await cloudFormationClient.send(new CreateStackCommand({
+    StackName: 'CDKToolkit',
+    TemplateBody: bootstrapTemplate,
+    Capabilities: ['CAPABILITY_NAMED_IAM'],
+  }));
 }
 
 void main();
