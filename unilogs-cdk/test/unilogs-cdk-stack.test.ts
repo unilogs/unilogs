2f86987bef62a971035cabc6a42fb17932ff9d87
astout55555 <alex.the.stout5@gmail.com>
2025-04-08 12:55:09 -0700

    feat: configured updated testing, set up test skeleton but instantiating stack raises error

--- unilogs-cdk/test/unilogs-cdk-stack.test.ts
+++ unilogs-cdk/test/unilogs-cdk-stack.test.ts
@@ -1,1 +1,11 @@
 import { Template } from 'aws-cdk-lib/assertions'; // don't use the outdated @aws-cdk/assertions package, which is for CDK v1
+import * as cdk from "aws-cdk-lib";
+import { UnilogsCdkStack } from "../lib/unilogs-cdk-stack";
+
+test("UniLogs Stack creates 3 S3 Buckets, 1 for bootstrap, 2 for Loki", () => {
+  const app = new cdk.App();
+  const stack = new UnilogsCdkStack(app, "UniLogsTestStack"); // causes error... needs review
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs("AWS::S3::Bucket", 3);
+});
