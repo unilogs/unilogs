0bd40616183514437d5d89e5322c471a76284146
Nathan Gross <nathan.gross@gmail.com>
2025-04-10 15:43:58 -0500

    fix: removing build/

--- configuration_generator/build/lib/buildAndRunShipper.js
+++
@@ -1,9 +0,0 @@
-import child_process from 'child_process';
-import { generateBuildImageCommand, generateRunImageCommand, } from './generateDockerCommands.js';
-function buildAndRunShipper(vectorConfiguration) {
-    child_process.execSync(generateBuildImageCommand(), { stdio: 'inherit' });
-    child_process.execSync(generateRunImageCommand(vectorConfiguration), {
-        stdio: 'inherit',
-    });
-}
-export default buildAndRunShipper;
