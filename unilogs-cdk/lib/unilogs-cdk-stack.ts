b3554bb1982f4f42531530650ee0bebe7f34c5b6
John <117869184+TamerJohn@users.noreply.github.com>
2025-04-15 21:19:50 +0300

    The labels work on the loki side, but the timestamps are ignored all together for the time that loki ingests them

--- unilogs-cdk/lib/unilogs-cdk-stack.ts
+++ unilogs-cdk/lib/unilogs-cdk-stack.ts
@@ -603,7 +603,9 @@ export class UnilogsCdkStack extends cdk.Stack {
               type: 'remap',
               inputs: ['kafka'],
               source: `
-                .inferred_label = .unilogs_service_label
+                . = parse_json!(.message)
+                inferred_label = .unilogs_service_label
+                .timestamp = parse_timestamp!(.timestamp, format: "%Y-%m-%dT%H:%M:%S.%3fZ")
               `.trim(),
             },
           },
@@ -614,8 +616,9 @@ export class UnilogsCdkStack extends cdk.Stack {
               endpoint: 'http://loki-gateway.loki.svc.cluster.local/',
               path: '/loki/api/v1/push',
               labels: {
-                unilogs_test_label: '{{`{{ inferred_label }}`}}',
+                service_name: '{{`{{ inferred_label }}`}}',
                 agent: 'vector',
+                level: '{{`({{ .level }})`}}'
               },
               tenant_id: 'default',
               encoding: {
@@ -627,6 +630,13 @@ export class UnilogsCdkStack extends cdk.Stack {
                 user: 'admin',
               },
             },
+            console: {
+              type: 'console',
+              inputs: ['parsed_logs'],
+              encoding: {
+                codec: 'json'
+              }
+            }
           },
         },
       },
