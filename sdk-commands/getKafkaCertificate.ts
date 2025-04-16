8e8cac7b273f1bfde29cf702fbbfbe92e56a2a30
Nathan Gross <nathan.gross@gmail.com>
2025-04-16 15:34:32 -0500

    feat: finished implementing getKafkaCertificate

---
+++ sdk-commands/getKafkaCertificate.ts
@@ -0,0 +1,48 @@
+import dotenv from 'dotenv';
+import { AwsCredentialIdentity } from '@smithy/types';
+import safeAssertString from './utils/safeAssertString';
+import { EKSClient, DescribeClusterCommand } from '@aws-sdk/client-eks';
+import fetch from "node-fetch";
+import https from 'https';
+dotenv.config();
+import EKSToken from 'aws-eks-token';
+
+async function main(clusterName: string) {
+  const token = await EKSToken.renew(clusterName);
+
+  safeAssertString(process.env.AWS_ACCESS_KEY_ID);
+  safeAssertString(process.env.AWS_SECRET_ACCESS_KEY);
+  safeAssertString(process.env.AWS_SESSION_TOKEN);
+  safeAssertString(process.env.AWS_REGION);
+  safeAssertString(process.env.AWS_ACCOUNT);
+  safeAssertString(process.env.AWS_USER);
+  const credentials: AwsCredentialIdentity = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN,
+    accountId: process.env.AWS_ACCOUNT,
+  };
+
+  const eksClient = new EKSClient({ credentials });
+  const clusterData = await eksClient.send(
+    new DescribeClusterCommand({ name: clusterName })
+  );
+
+  const endpoint = clusterData.cluster?.endpoint?.toLowerCase() as string;
+  const caCert = Buffer.from(clusterData.cluster?.certificateAuthority
+    ?.data ?? '', 'base64').toString('utf8');
+    
+  const secretUrl = `${endpoint}/api/v1/namespaces/kafka/secrets/kafka-tls`;
+  const res = await fetch(secretUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json'
+    },
+    // In production, verify with the CA cert properly
+    agent: new https.Agent({ca: caCert})
+  });
+  const body = await res.json() as {data: {'ca.crt': string}};
+  console.log(Buffer.from(body.data['ca.crt'], 'base64').toString('utf8')); 
+}
+void main('unilogs-cluster');
